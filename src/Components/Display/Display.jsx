61cf2b75371a611913a85a93ac6ffb5e0237b4ac
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-09-03 22:36:31 +0530

    done done done done done done

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -6,7 +6,7 @@ const Display = () => {
             <div className=" py-[33.5px] bg-[#F9FAFC] border-b-2 ">
 
             </div>
-            <div className=" p-10 px-32">
+            <div className=" p-5 md:p-10 md:px-32">
                 <h1 className=" font-[poppins] font-bold text-[42px]"> Introduction </h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ducimus autem repellendus ratione amet reprehenderit voluptas atque omnis, dicta ullam molestias expedita corporis tenetur eveniet quibusdam pariatur, hic laudantium aperiam!</p>
             </div>
