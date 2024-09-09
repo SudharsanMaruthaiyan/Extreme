a14eb0a8f563fa32a9d2dac1dc77724334ed13fc
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2024-09-09 21:28:00 +0530

    zxxczxc

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Display from './Components/Display/Display';
 import Introducion from './Components/Week1Component/Introduction/Introducion';
 import Features from './Components/Week1Component/Features/Features';
 import Usage from './Components/Week1Component/Usage/Usage';
 import Interaction from './Components/Week1Component/Interaction/Interaction';
 import Preparation from './Components/Week1Component/Preparation/Preparation';
 import Week1quiz from './Components/Week1Component/Week1quiz/Week1quiz';
-import Demo from './Components/demo';
 const Applayout = ()=>{
   return(
     <>
